73a28cca8b84c2e2eadbaff00ea53295dc1c4639
Ross Hettel <rosshettel@gmail.com>
2011-09-26 02:09:19 -0500

    made the calculate button do something, as well as the clear button

--- A2 - Mortgage Calculator/src/edu/niu/cs/rosshettel/mortgage/MortgageCalculatorActivity.java
+++ A2 - Mortgage Calculator/src/edu/niu/cs/rosshettel/mortgage/MortgageCalculatorActivity.java
@@ -4,15 +4,58 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MortgageCalculatorActivity extends Activity {
+	EditText principle, interest, term;
+	Button calculate, clear;
+	TextView output_mortgage, output_repayment, output_error;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        principle = (EditText) findViewById(R.id.mortgage_input);
+        interest = (EditText) findViewById(R.id.interest_input);
+        term = (EditText) findViewById(R.id.term_input);
         
+        calculate = (Button) findViewById(R.id.calculate_button);
+        
+        output_mortgage = (TextView) findViewById(R.id.output_mortgage);
+        output_repayment = (TextView) findViewById(R.id.output_repayment);
+        output_error = (TextView) findViewById(R.id.output_error);
+    }
+    
+    /****************************************************************
+		FUNCTION:   void calculateMortgage(View theButton)
+		ARGUMENTS:  view
+		RETURNS:    none
+		NOTES:      Get all the user inputs, convert to respective data types,
+					then calculate the mortgage and output.
+     ****************************************************************/
+    public void calculateMortgage(View theButton) {
+    	double principle_num = 0;
+    	double interest_num = 0;
+    	double result;
+    	int term_num = 0;
+    	
+    	try {
+    		principle_num = Double.parseDouble(principle.getText().toString());
+    		interest_num = Double.parseDouble(interest.getText().toString());
+    		term_num = Integer.parseInt(term.getText().toString());
+    	} catch (final NumberFormatException e) {
+    		Log.d("A2_debug", "We encountered an error parsing user input.");
+    		// let the user know the input is wrong
+    		output_error.setText("Warning - There was a problem parsing user input, please try again.");
+    	}
+    	
+    	result = calculateMonthlyPayment(principle_num, interest_num, term_num);
+    	
+    	output_mortgage.setText(Double.toString(result));
     }
     
     /*
@@ -48,5 +91,9 @@ public class MortgageCalculatorActivity extends Activity {
      */
     public void clearInputs(View theButton) {
     	//wipe it all clean
+    	principle.setText("");
+    	interest.setText("");
+    	term.setText("");
+    	Log.d("A2_debug", "Cleared all the inputs.");
     }
 }
