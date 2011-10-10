87a11eddd0a2eb329e9e511f461088f61963ec9f
John Miller <jdmlight@gmail.com>
2011-10-10 15:13:52 -0500

    copied calculate total/monthly payment functions from A2

--- A3 - Tabbed Calculator/src/edu/niu/cs/rosshettel/Assignment3/A3TabbedCalculatorActivity.java
+++ A3 - Tabbed Calculator/src/edu/niu/cs/rosshettel/Assignment3/A3TabbedCalculatorActivity.java
@@ -2,6 +2,7 @@ package edu.niu.cs.rosshettel.Assignment3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class A3TabbedCalculatorActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +11,28 @@ public class A3TabbedCalculatorActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    /****************************************************************
+		FUNCTION:   double calculateMonthlyPayment(double, double, int)
+		ARGUMENTS:  principle (double), annual interest rate (double), term in months (int)
+		RETURNS:    monthly payment (double)
+		NOTES:      This calculates the monthly payment for a mortgage.
+     ****************************************************************/
+    public double calculateMonthlyPayment(double principle, double annualInterestRate, int numMonths) 
+		{
+			double monthlyInterestRate = annualInterestRate/12;
+			Log.d("A2_debug", "monthlyInterestRate: " + monthlyInterestRate);
+			double monthlyPmt = principle*monthlyInterestRate/(1-Math.pow((1+monthlyInterestRate),(-numMonths)));
+			return monthlyPmt;
+		}
+    /****************************************************************
+		FUNCTION:   double calculateTotalRepayment(double, int)
+		ARGUMENTS:  monthly payment (double), term in months (int)
+		RETURNS:    total amount of repayment (double)
+		NOTES:      This calculates the total repayment on a mortgage.
+     ****************************************************************/
+    public double calculateTotalRepayment(double monthlyPayment, int numMonths)
+    	{
+    		return monthlyPayment*numMonths;
+    	}
 }
