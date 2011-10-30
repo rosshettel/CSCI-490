60539fdf836c8027dabcf99a318104d1d605c5ec
John Miller <jdmlight@gmail.com>
2011-10-30 18:27:32 -0500

    Made menu buttons put something in the log.

--- A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/StartActivity.java
+++ A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/StartActivity.java
@@ -3,7 +3,9 @@ package edu.niu.cs.rosshettel.Assignment4;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 //test
@@ -26,5 +28,24 @@ public class StartActivity extends Activity {
     	MenuInflater inflater = getMenuInflater();
     	inflater.inflate(R.menu.menu, passedMenu);
     	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+    	switch (item.getItemId())
+    	{
+    	case R.id.about:
+    		Log.d("A4_debug", "About selected");
+    		return true;
+    	case R.id.study:
+    		Log.d("A4_debug", "Study selected");
+    		return true;
+    	case R.id.take_quiz:
+    		Log.d("A4_debug", "Take quiz selected");
+    		return true;
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	}
     }
 }
