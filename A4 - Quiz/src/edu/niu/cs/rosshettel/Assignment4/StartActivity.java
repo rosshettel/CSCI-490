f6fbc6bd0ab916357581e6cec6c707c022530c87
Missingno <z159807@students.niu.edu>
2011-10-30 15:44:55 -0500

    my attempt at making the title screen. I am not sure if this is what he is looking for. buttons don't do anything yet.

--- A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/StartActivity.java
+++ A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/StartActivity.java
@@ -1,13 +1,27 @@
 package edu.niu.cs.rosshettel.Assignment4;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 //test
 public class StartActivity extends Activity {
+    TextView title;
+    Button takeQuiz, surfWeb, aboutButton;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Resources res = getResources();
+        
+        //make a label of this
+        title = (TextView) findViewById(R.id.title_message);
+        takeQuiz = (Button) findViewById(R.id.quiz_button);
+        surfWeb = (Button) findViewById(R.id.internet_button);
+        aboutButton = (Button) findViewById(R.id.about_button);
+        
     }
 }
