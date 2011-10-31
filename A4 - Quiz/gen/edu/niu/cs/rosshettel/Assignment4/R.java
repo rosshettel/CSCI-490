dcad6bc0baa1f3419e9d21c1d911ef839a3e05d7
John Miller <jdmlight@gmail.com>
2011-10-30 20:28:26 -0500

    Alex added the internet. Yes, the whole internet.

--- A4 - Quiz/gen/edu/niu/cs/rosshettel/Assignment4/R.java
+++ A4 - Quiz/gen/edu/niu/cs/rosshettel/Assignment4/R.java
@@ -17,14 +17,16 @@ public final class R {
         public static final int test=0x7f020003;
     }
     public static final class id {
-        public static final int about=0x7f070004;
-        public static final int mainLayout=0x7f070000;
-        public static final int study=0x7f070003;
-        public static final int take_quiz=0x7f070002;
-        public static final int title_message=0x7f070001;
+        public static final int about=0x7f070005;
+        public static final int mainLayout=0x7f070001;
+        public static final int study=0x7f070004;
+        public static final int take_quiz=0x7f070003;
+        public static final int title_message=0x7f070002;
+        public static final int webkit=0x7f070000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int internet=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
         public static final int menu=0x7f060000;
---
+++ A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/Internet.java
@@ -0,0 +1,20 @@
+package edu.niu.cs.rosshettel.Assignment4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+//test
+public class Internet extends Activity {
+    WebView bowser;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.internet);
+        bowser=(WebView)findViewById(R.id.webkit);
+        
+        bowser.loadUrl("http://www.seasite.niu.edu/cs680Android/default.html");
+        
+    }
+}
