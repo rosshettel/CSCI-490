6f9b4e9709469998275b69545d44595eb5998312
Missingno <z159807@students.niu.edu>
2011-11-01 19:09:19 -0500

    added comments

--- A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/Internet.java
+++ A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/Internet.java
@@ -1,3 +1,20 @@
+/****************************************************************
+   PROGRAM:   Assignment 4
+   AUTHOR:    Ross Hettel, John Miller, Alex Wohead
+   LOGON ID:  Z1549355, Z159807, Z1624450
+   DUE DATE:  11/02 at class time
+
+   FUNCTION:  This the Internet class creates an Internet object
+              with the url set to a wikipedia article on ipod's.
+              This function is used when the user wishes to study
+              for the quiz.
+
+   INPUT:     NONE
+
+   OUTPUT:    It pops up the ipod wikipedia article
+
+   NOTES:     None.
+****************************************************************/
 package edu.niu.cs.rosshettel.Assignment4;
 
 import android.app.Activity;
@@ -5,8 +22,9 @@ import android.os.Bundle;
 import android.webkit.WebView;
 //test
 public class Internet extends Activity {
+    
     WebView bowser;
-	
+      
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -14,6 +32,7 @@ public class Internet extends Activity {
         setContentView(R.layout.internet);
         bowser=(WebView)findViewById(R.id.webkit);
         
+        //make the browser show the wikipedia article on ipods
         bowser.loadUrl("http://en.wikipedia.org/w/index.php?title=IPod&useformat=mobile");
         
     }
