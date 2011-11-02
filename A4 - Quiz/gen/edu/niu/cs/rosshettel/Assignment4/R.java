1ef63190238ed105ad6fc21cb2a90f6f68474e66
John Miller <jdmlight@gmail.com>
2011-11-01 21:44:38 -0500

    added a bunch o'junk to get xmlpullparser to do stuff

--- A4 - Quiz/gen/edu/niu/cs/rosshettel/Assignment4/R.java
+++ A4 - Quiz/gen/edu/niu/cs/rosshettel/Assignment4/R.java
@@ -17,26 +17,27 @@ public final class R {
         public static final int test=0x7f020003;
     }
     public static final class id {
-        public static final int about=0x7f070015;
+        public static final int about=0x7f070016;
         public static final int about_content=0x7f070001;
-        public static final int button1=0x7f070011;
-        public static final int button2=0x7f070012;
+        public static final int button1=0x7f070012;
+        public static final int button2=0x7f070013;
         public static final int linearLayout1=0x7f070000;
         public static final int mainLayout=0x7f070003;
-        public static final int radioButton1=0x7f070009;
-        public static final int radioButton2=0x7f07000b;
-        public static final int radioButton3=0x7f07000d;
-        public static final int radioButton4=0x7f07000f;
-        public static final int study=0x7f070014;
-        public static final int tableLayout1=0x7f070005;
-        public static final int tableRow1=0x7f070006;
-        public static final int tableRow2=0x7f070008;
-        public static final int tableRow3=0x7f07000a;
-        public static final int tableRow4=0x7f07000c;
-        public static final int tableRow5=0x7f07000e;
-        public static final int tableRow6=0x7f070010;
-        public static final int take_quiz=0x7f070013;
-        public static final int textView1=0x7f070007;
+        public static final int radioButton1=0x7f07000a;
+        public static final int radioButton2=0x7f07000c;
+        public static final int radioButton3=0x7f07000e;
+        public static final int radioButton4=0x7f070010;
+        public static final int radioGroup1=0x7f070005;
+        public static final int study=0x7f070015;
+        public static final int tableLayout1=0x7f070006;
+        public static final int tableRow1=0x7f070007;
+        public static final int tableRow2=0x7f070009;
+        public static final int tableRow3=0x7f07000b;
+        public static final int tableRow4=0x7f07000d;
+        public static final int tableRow5=0x7f07000f;
+        public static final int tableRow6=0x7f070011;
+        public static final int take_quiz=0x7f070014;
+        public static final int textView1=0x7f070008;
         public static final int title_message=0x7f070004;
         public static final int webkit=0x7f070002;
     }
--- A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/Quiz.java
+++ A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/Quiz.java
@@ -8,23 +8,39 @@ import org.xmlpull.v1.XmlPullParserException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Quiz extends Activity {
 	//my fancy way of making a global log tag
-	private final String LOG_TAG = getResources().getString(R.string.logtag);
+	//private final String LOG_TAG = getResources().getString(R.string.logtag);
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.quiz);
+	    loadXML();
 	
 	}
 	
-	public boolean loadXML(int quizXML)
+	public boolean loadXML()
 	{
 		XmlPullParser xpp;
-		xpp = getResources().getXml(quizXML);
+		xpp = getResources().getXml(R.xml.questions);
+		TextView text = new TextView(this);
+		text = (TextView)findViewById(R.id.textView1);
+		RadioGroup radioG = (RadioGroup)findViewById(R.id.radioGroup1);
+		RadioButton radio1 = new RadioButton(this);
+		radio1 = (RadioButton)findViewById(R.id.radioButton1);
+		RadioButton radio2 = new RadioButton(this);
+		radio2 = (RadioButton)findViewById(R.id.radioButton2);
+		RadioButton radio3 = new RadioButton(this);
+		radio3 = (RadioButton)findViewById(R.id.radioButton3);
+		RadioButton radio4 = new RadioButton(this);
+		radio4 = (RadioButton)findViewById(R.id.radioButton4);
+		
 		try{
 			//lets load the quiz data from the xml file
 			while (xpp.getEventType() != XmlPullParser.END_DOCUMENT)
@@ -32,23 +48,27 @@ public class Quiz extends Activity {
 				if(xpp.getEventType() == XmlPullParser.START_TAG)
 				{
 					if(xpp.getName().equals("question"))
-					Log.i(LOG_TAG, (String)xpp.getAttributeName(1));
-					Log.i(LOG_TAG, (String)xpp.getAttributeValue(1));
-					
-					Log.i(LOG_TAG, (String)xpp.getAttributeName(2));
-					Log.i(LOG_TAG, (String)xpp.getAttributeValue(2));
+					{
+						text.setText((String)xpp.getAttributeValue(1));
+						radio1.setText((String)xpp.getAttributeValue(2));
+						Log.i("A4_debug", (String)xpp.getAttributeName(1));
+						Log.i("A4_debug", (String)xpp.getAttributeValue(1));	
+						Log.i("A4_debug", (String)xpp.getAttributeName(2));
+						Log.i("A4_debug", (String)xpp.getAttributeValue(2));
+						//radio button listener here possibly
+					}
 				}
 				
 				try {
 					xpp.next();
 				} catch (IOException e) {
-					Log.e(LOG_TAG, "IOException: " + e.getMessage());
+					Log.e("A4_debug", "IOException: " + e.getMessage());
 				}
 			}
 		}
 		catch(XmlPullParserException e)
 		{
-			Log.e(LOG_TAG, "XmlPullParserException: " + e.getMessage());
+			Log.e("A4_debug", "XmlPullParserException: " + e.getMessage());
 			//maybe display a toast message here as well?
 			return false;
 		}
