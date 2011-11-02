e46a7f912d07a6b0c78cb706810e5d3b1a18cbe0
Missingno <z159807@students.niu.edu>
2011-11-01 20:12:43 -0500

    Update A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/About.java

--- A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/About.java
+++ A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/About.java
@@ -1,9 +1,33 @@
+/****************************************************************
+   PROGRAM:   Assignment 3 part 2
+   AUTHOR:    Ross Hettel, John Miller, Alex Wohead
+   LOGON ID:  Z1549355, Z159807, Z1624450
+   DUE DATE:  10/17 at class time
+
+   FUNCTION:  The About class simply sets the content view
+              to the About page of the program. The details
+              are inside the xml.
+
+   INPUT:     None.
+
+   OUTPUT:    The About page of the program.
+
+   NOTES:     None.
+****************************************************************/
+
 package edu.niu.cs.rosshettel.Assignment4;
 
 import android.app.Activity;
 import android.os.Bundle;
 
 public class About extends Activity {
+     /****************************************************************
+        	FUNCTION:   void onCreate(Bundle)
+		ARGUMENTS:  saved instance state (Bundle)
+		RETURNS:    nothing
+		NOTES:      This simply changes the layout to the about
+                            page. Thats it.
+     ****************************************************************/
         @Override
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
