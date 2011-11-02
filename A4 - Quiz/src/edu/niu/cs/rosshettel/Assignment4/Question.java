c9568b5057ae6b90043f10febd3eed0fdf290c86
Ross Hettel <rosshettel@gmail.com>
2011-11-02 15:43:53 -0500

    added comments to question.java

--- A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/Question.java
+++ A4 - Quiz/src/edu/niu/cs/rosshettel/Assignment4/Question.java
@@ -1,5 +1,21 @@
-package edu.niu.cs.rosshettel.Assignment4;
+/****************************************************************
+   PROGRAM:   Assignment 4
+   AUTHOR:    Ross Hettel, John Miller, Alex Wohead
+   LOGON ID:  Z1549355, Z159807, Z1624450
+   DUE DATE:  11/02 at class time
 
+   FUNCTION:  The Question class is an object that holds the question text,
+   			  possible answers, and correct answer. Also includes a function to check
+   			  if the submitted answer is the right one.
+
+   INPUT:     none
+
+   OUTPUT:    none
+
+   NOTES:     The get and set methods are self-explantory and therefore do not
+   			  contain much commenting.
+****************************************************************/
+package edu.niu.cs.rosshettel.Assignment4;
 
 public class Question {
 	String questionText;
@@ -36,6 +52,7 @@ public class Question {
 		rightAnswer = answer;
 	}
 	
+	/* check answer if it's right and return the appropriate bool */
 	public boolean checkAnswer(int answer)
 	{
 		if(answer == rightAnswer)
