63d3af4672abe894d4a2fa5a49146d9d79dd30fc
Ross Hettel <rosshettel@gmail.com>
2011-11-22 21:45:35 -0600

    first layout for thread app

---
+++ A6 - Threads/src/com/edu/niu/cs/rosshettel/A6Threads/ThreadsActivity.java
@@ -0,0 +1,13 @@
+package com.edu.niu.cs.rosshettel.A6Threads;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThreadsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
