2541f40455ae3e1f98900bd64a86f71b4e9304a9
Ross Hettel <rosshettel@gmail.com>
2011-11-24 00:57:05 -0600

    got the clock thread running

--- A6 - Threads/gen/com/edu/niu/cs/rosshettel/A6Threads/R.java
+++ A6 - Threads/gen/com/edu/niu/cs/rosshettel/A6Threads/R.java
@@ -23,22 +23,16 @@ public final class R {
         public static final int ProgressBar7=0x7f050007;
         public static final int ProgressBar8=0x7f050008;
         public static final int ProgressBar9=0x7f050009;
-        public static final int cardDisplay=0x7f050012;
-        public static final int clockText=0x7f050018;
-        public static final int flipButton=0x7f050015;
-        public static final int nextButton=0x7f050016;
+        public static final int cardDisplay=0x7f05000d;
+        public static final int clockText=0x7f050012;
+        public static final int flipButton=0x7f05000f;
+        public static final int nextButton=0x7f050010;
         public static final int progressBar10=0x7f05000a;
         public static final int progressBars=0x7f050000;
-        public static final int reStartButton=0x7f050014;
-        public static final int removeButton=0x7f050017;
-        public static final int resetButton=0x7f05000e;
-        public static final int sortButton=0x7f05000d;
-        public static final int tableLayout1=0x7f05000b;
-        public static final int tableRow1=0x7f05000c;
-        public static final int tableRow3=0x7f05000f;
-        public static final int tableRow4=0x7f050011;
-        public static final int tableRow5=0x7f050013;
-        public static final int textView2=0x7f050010;
+        public static final int reStartButton=0x7f05000e;
+        public static final int removeButton=0x7f050011;
+        public static final int resetButton=0x7f05000c;
+        public static final int sortButton=0x7f05000b;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- A6 - Threads/src/com/edu/niu/cs/rosshettel/A6Threads/ThreadsActivity.java
+++ A6 - Threads/src/com/edu/niu/cs/rosshettel/A6Threads/ThreadsActivity.java
@@ -1,13 +1,32 @@
 package com.edu.niu.cs.rosshettel.A6Threads;
 
+import java.util.Date;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class ThreadsActivity extends Activity {
 	private static int barCount = 10;
+	private static String logtag = "A6";
+	TextView clockText;
+	boolean isRunning = false;
+	Date date;
+	
+	Handler clockThread = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			date = new Date();
+			Log.d(logtag, "in handleMessage() - " + date.toString());
+			clockText.setText(date.toString());
+		}
+	};
+	
+	
 	
     /** Called when the activity is first created. */
     @Override
@@ -25,18 +44,46 @@ public class ThreadsActivity extends Activity {
         
         
         //lets set up the clock thread
-        TextView clockText = (TextView)findViewById(R.id.clockText);
+        clockText = (TextView)findViewById(R.id.clockText);
+        date = new Date();
         
-        Thread clock = new Thread(new Runnable()
-        {
-        	public void run()
-        	{
-        		
-        	}
-        });
-        
-        
-        
-        
+    }
+    
+    public void onResume() {
+    	super.onResume();
+    	clockText.setText(date.toString());
+    	Log.d(logtag, "in onResume()");
+    	    	
+    	Thread clock = new Thread(new Runnable() {
+    		public void run() {
+    			try {
+    				//only do this while isRunning is true - aka we haven't hit onPause or onStop yet
+    				while(isRunning)
+    				{
+    					Thread.sleep(1000); //wait one second
+    					//call the clockThread handler
+    					clockThread.sendMessage(clockThread.obtainMessage());
+    				}
+				} catch (Throwable t) {
+					Log.d(logtag, "we've reached the catch of the clock thread try/catch | " + t.getLocalizedMessage());
+					//just end the clock thread
+				}
+    		}
+    	});
+    	
+    	isRunning = true;
+    	clock.start();
+    }
+    
+    public void onPause() {
+    	Log.d(logtag, "in onPause()");
+    	super.onPause();
+    	isRunning = false;
+    }
+    
+    public void onStop() {
+    	Log.d(logtag, "in onStop()");
+    	super.onStop();
+    	isRunning = false;
     }
 }
