891e441687b9a4a2820b3f972852482ed75e7866
Ross Hettel <rosshettel@gmail.com>
2011-11-28 23:42:04 -0600

    increased the text size just a bit

--- A6 - Threads/gen/com/edu/niu/cs/rosshettel/A6Threads/R.java
+++ A6 - Threads/gen/com/edu/niu/cs/rosshettel/A6Threads/R.java
@@ -14,25 +14,25 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int ProgressBar1=0x7f060001;
-        public static final int ProgressBar2=0x7f060002;
-        public static final int ProgressBar3=0x7f060003;
-        public static final int ProgressBar4=0x7f060004;
-        public static final int ProgressBar5=0x7f060005;
-        public static final int ProgressBar6=0x7f060006;
-        public static final int ProgressBar7=0x7f060007;
-        public static final int ProgressBar8=0x7f060008;
-        public static final int ProgressBar9=0x7f060009;
-        public static final int cardDisplay=0x7f06000d;
-        public static final int clockText=0x7f060012;
-        public static final int flipButton=0x7f06000f;
-        public static final int nextButton=0x7f060010;
-        public static final int progressBar10=0x7f06000a;
-        public static final int progressBars=0x7f060000;
-        public static final int reStartButton=0x7f06000e;
-        public static final int removeButton=0x7f060011;
-        public static final int resetButton=0x7f06000c;
-        public static final int sortButton=0x7f06000b;
+        public static final int ProgressBar1=0x7f070001;
+        public static final int ProgressBar2=0x7f070002;
+        public static final int ProgressBar3=0x7f070003;
+        public static final int ProgressBar4=0x7f070004;
+        public static final int ProgressBar5=0x7f070005;
+        public static final int ProgressBar6=0x7f070006;
+        public static final int ProgressBar7=0x7f070007;
+        public static final int ProgressBar8=0x7f070008;
+        public static final int ProgressBar9=0x7f070009;
+        public static final int cardDisplay=0x7f07000d;
+        public static final int clockText=0x7f070012;
+        public static final int flipButton=0x7f07000f;
+        public static final int nextButton=0x7f070010;
+        public static final int progressBar10=0x7f07000a;
+        public static final int progressBars=0x7f070000;
+        public static final int reStartButton=0x7f07000e;
+        public static final int removeButton=0x7f070011;
+        public static final int resetButton=0x7f07000c;
+        public static final int sortButton=0x7f07000b;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -40,6 +40,9 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
+    }
+    public static final class style {
+        public static final int Buttons=0x7f060000;
     }
     public static final class xml {
         public static final int flipcards=0x7f040000;
